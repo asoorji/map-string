dd3018ed0af1c0a90430398e906a357bcdcb9d1f
Aso Orji <42071241+asoorji@users.noreply.github.com>
2023-11-14 13:58:21 +0100

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+main() {
+ 
+  Map dataMap = {
+    'name': "Bisi",
+    'age': "27",
+    'location': "Lagos",
+  };  
+ 
+ String dataString =jsonEncode(dataMap);
+  
+  Map dataMap2 = jsonDecode(dataString);
+ 
+    print(dataMap);
+  print(dataMap[3]);
+    print(dataMap['age']);
+
+  
+    print(dataString);
+  print(dataString[3]);
+  
+    print(dataMap2);
+  print(dataMap2[3]);
+}
